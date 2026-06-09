60ef4f356c4b652190c9746c474f6e9396bbc386
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-06-09 09:00:58 +0530

    Time: 124 ms (6.9%), Space: 66.4 MB (43.78%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -5,12 +5,11 @@
 let nums = [1,3,4,2,2]
 // Output: 2
 var findDuplicate = function(nums) {   
-     let set = new Set;
-    for(let i=0;i<nums.length;i++){
-        if(set.has(nums[i])){
-            return nums[i]
-        }
-        set.add(nums[i])     
+    nums.sort((a,b)=>a-b); 
+    for(i=0;i<nums.length;i++){
+        if(nums[i]==nums[i+1]){
+            return nums[i];
+        }       
     }
     return -1
 };
