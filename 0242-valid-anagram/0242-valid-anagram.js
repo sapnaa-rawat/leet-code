5fcb4e7cd261d0862e5293d4c73068029000e143
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-06-15 11:32:33 +0530

    Time: 24 ms (22.78%), Space: 58.4 MB (39.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,12 +3,15 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {    
+var isAnagram = function(s, t) {  
+    if(s.length !=t.length)  {
+        return false
+    }
     let v1= s.split('').sort().join('')
     console.log(v1)
     let v2= t.split('').sort().join('')
     console.log(v2)
-    if(v1===v2&&v1.length===v2.length){
+    if(v1===v2){
         return true
     }else
     return false
