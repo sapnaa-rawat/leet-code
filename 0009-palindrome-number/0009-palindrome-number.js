b273a33d954d573ef48f6b0ebbb32ff09f37352d
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-06-26 11:17:57 +0530

    Time: 5 ms (77.84%), Space: 64.4 MB (46.43%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,26 +2,21 @@
  * @param {number} x
  * @return {boolean}
  */
-  
- const check = (clean)=>{
+
+var isPalindrome = function(x) {
+    let newString = x.toString();
+    if(newString<0){
+        return false
+    }
     let left =0;
-    let right = clean.length-1;
+    let right = newString.length-1;
     while(left<=right){
-        if(clean[left]!==clean[right]){
+        if(newString[left]!==newString[right]){
             return false;
         }
         left++;
         right--;
     }
     return true;
- }
-var isPalindrome = function(x) {
-    let newString = x.toString();
-    if(newString<0){
-        return false
-    }
-    let clean = newString.replace(/[^A-Za-z0-9]/g,'')
-
-    return check(clean)
-
+    
 };
