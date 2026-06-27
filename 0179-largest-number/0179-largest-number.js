ed815711b06a01041b0441b62b4b2032b1a23981
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-06-27 10:01:33 +0530

    Time: 3 ms (63.09%), Space: 56.3 MB (42.15%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -4,8 +4,15 @@
  */
 var largestNumber = function(nums) {
     
-    let sorted = nums.sort((a,b)=>
-    ((b.toString() + a.toString())-( a.toString() + b.toString())))
+    let sorted = nums.sort((a,b)=>{
+        let ba = b.toString() + a.toString()
+        let ab = a.toString() + b.toString()
+    if(ba>ab) return 1
+    if(ab>ba) return -1
+    return 0
+    })
+    
+
     let output = sorted.join("")
     if(output[0]==="0"){
         return "0"
