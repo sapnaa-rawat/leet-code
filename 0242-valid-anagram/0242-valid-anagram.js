7cf5f80506745e680935a68dc42361fd8091216a
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-06-29 08:13:44 +0530

    Time: 17 ms (39.85%), Space: 58.2 MB (40.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,16 +3,36 @@
  * @param {string} t
  * @return {boolean}
  */
+//  if(s.length !=t.length)  {
+//         return false
+//     }
+//     let v1= s.split('').sort().join('')
+//     console.log(v1)
+//     let v2= t.split('').sort().join('')
+//     console.log(v2)
+//     if(v1===v2){
+//         return true
+//     }else
+//     return false
 var isAnagram = function(s, t) {  
-    if(s.length !=t.length)  {
-        return false
+    if(s.length!=t.length){
+        return false;
     }
-    let v1= s.split('').sort().join('')
-    console.log(v1)
-    let v2= t.split('').sort().join('')
-    console.log(v2)
-    if(v1===v2){
-        return true
-    }else
-    return false
-};
+    let seen = new Map();
+    for(let char of s){
+        if(seen.has(char)){
+            seen.set(char,seen.get(char)+1)
+        }else
+        seen.set(char,1)
+    }
+    for(let char of t){
+        if(!seen.has(char)){
+            return false
+        }
+        seen.set(char,seen.get(char)-1)
+        if(seen.get(char)<0){
+            return false;
+        }
+    }
+    return true
+}
